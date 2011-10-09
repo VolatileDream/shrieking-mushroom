b23152c9f0db4b19e83d1344dbe28855037c683b
Gianni Gambetti <Gianni@Gambetti.ca>
2011-10-09 18:56:49 -0400

    Fixed up TCP Server javadoc

--- src/networking/TCPServer.java
+++ src/networking/TCPServer.java
@@ -7,7 +7,7 @@ public interface TCPServer extends IRunner {
 	/**
 	 * Reopens the port that the tcp server was listening on.
 	 * <br>Will error if stop hasn't been called.
-	 * <br>It will wait for the TCP server to stop from the previous stop call before restarting.
+	 * <br>The call will wait for the TCP server to stop from the previous stop call before restarting.
 	 */
 	public void restart();
 	
